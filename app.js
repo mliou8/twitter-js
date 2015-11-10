e89e85577c0f62a001e93796bb9e388f6254ea87
Yustynn <yustynn@gmail.com>
2015-11-10 15:23:12 -0500

    Add logging middleware

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-var express = require('express');
-var app = express();
-var chalk = require('chalk');
+// MODULES
+var express = require('express'),
+	app = express(),
+	chalk = require('chalk'),
+	morgan = require('morgan');
 
+// app.use(morgan('default', {}));
 app.use (function (req, res, next) {
-	console.log(chalk.red("This is the middleware"));
-	setInterval(function () { next();}, 3000);
+	res.statusCode = 418;
+	console.log(chalk.yellow([res.statusCode, req.method, req.path].join(' ')));
+	next();
 });
 
 app.listen(3000, function () {
@@ -14,6 +18,3 @@ app.listen(3000, function () {
 app.get('/', function (req, res) {
 	res.send('<h3> Hello, welcome to the world. </h3>');
 });
-
-
-
