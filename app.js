e317998140a1b08478d5258b061c2df9a9abf5d7
mliou8 <chunghanliou@gmail.com>
2015-11-10 15:07:39 -0500

    Added use

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var app = express();
+var chalk = require('chalk');
+
+app.use (function (req, res, next) {
+	console.log(chalk.red("This is the middleware"));
+	setInterval(function () { next();}, 3000);
+});
 
 app.listen(3000, function () {
-	console.log("Server listening")
+	console.log(chalk.red("Server listening"));
 });
 
 app.get('/', function (req, res) {
 	res.send('<h3> Hello, welcome to the world. </h3>');
-	console.log(req._headers.statusCode);
 });
+
+
+
